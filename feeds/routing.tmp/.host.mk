54bc9adb49528285270dbe5cbc432f7d7d34b4c8
minghua <minghua@localhost>
2013-10-25 18:08:05 -0700

    openwrt attitude adjustment feeds as of 2013-10-19 after feeds update -a

---
+++ feeds/routing.tmp/.host.mk
@@ -0,0 +1,9 @@
+HOST_OS:=Linux
+HOST_ARCH:=i686
+GNU_HOST_NAME:=i686-redhat-linux
+TAR:=/usr/bin/gtar
+FIND:=/usr/bin/find
+BASH:=/usr/bin/bash
+FIND_L=/usr/bin/find -L $(1)
+XARGS:=xargs -r
+PATCH:=/usr/bin/patch
